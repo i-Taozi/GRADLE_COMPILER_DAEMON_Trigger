ec167cc1277c6b333d4d6806258f96b25ffbd24d
i-Taozi <879714748@qq.com>
2022-02-18 22:50:35 +0800

    stephenh/joist-java:original code

--- app/src/main/java/info/nightscout/androidaps/events/EventTreatmentChange.kt
+++
@@ -1,5 +0,0 @@
-package info.nightscout.androidaps.events
-
-import info.nightscout.androidaps.plugins.treatments.Treatment
-
-class EventTreatmentChange(val treatment: Treatment?) : EventLoop()
--- app/src/main/java/info/nightscout/androidaps/plugins/general/nsclient/events/EventNSClientStatus.kt
+++
@@ -1,7 +0,0 @@
-package info.nightscout.androidaps.plugins.general.nsclient.events
-
-import info.nightscout.androidaps.events.EventStatus
-
-class EventNSClientStatus(var text: String) : EventStatus() {
-    override fun getStatus(): String = text
-}
